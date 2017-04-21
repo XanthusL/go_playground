04cb579806cf7492213ceeb9ad208bed39093e20
李谊恒 <liyihenggnehiyil@126.com>
2017-04-21 09:26:27 +0800

    Optimize: Use internal package to separate interface and implementation

--- archive/interf_separate/animal.go
+++ archive/interf_separate/animal.go
@@ -1,10 +1,14 @@
 package interf_separate
 
-import _ "lyh_demos/archive/interf_separate/inner"
+import "lyh_demos/archive/interf_separate/internal"
 
 // 只对外提供接口，具体实现在inner包，不导出，屏蔽实现细节
 type Animal interface {
 	Say() string
+}
+
+func init() {
+	Pet = &internal.Dog{}
 }
 
 // 对外提供接口类型
