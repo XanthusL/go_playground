a057fe2dd35b994958d0c0fcb5ac5e63cabc2341
李谊恒 <liyihenggnehiyil@126.com>
2017-04-21 17:41:18 +0800

    Comment add: More results for atomic demo.

--- archive/atomic/atomic_demo.go
+++ archive/atomic/atomic_demo.go
@@ -13,10 +13,30 @@ func RunTheTest() {
 	demo1() // Wrong result
 	demo2() // Correct
 	demo3() // Correct
-	// Result
-	//++ directly: 9113 time cost(nano): 7572762
-	//atomic: 10000 time cost(nano): 1814050
-	//Lock: 10000 time cost(nano): 2309760
+	// Result 0
+	// ++ directly: 9113 time cost(nano): 7572762
+	// atomic: 10000 time cost(nano): 1814050
+	// Lock: 10000 time cost(nano): 2309760
+
+	// Result 1
+	// ++ directly: 8879 time cost(nano): 2337648
+	// atomic: 10000 time cost(nano): 1960095
+	// Lock: 10000 time cost(nano): 2103779
+
+	// Result 2
+	// ++ directly: 8716 time cost(nano): 2314949
+	// atomic: 10000 time cost(nano): 5774471
+	// Lock: 10000 time cost(nano): 2461061
+
+	// Result 3
+	// ++ directly: 9155 time cost(nano): 2231489
+	// atomic: 10000 time cost(nano): 1949282
+	// Lock: 10000 time cost(nano): 3570333
+
+	// Result 4
+	// ++ directly: 9999 time cost(nano): 1496126
+	// atomic: 10000 time cost(nano): 6082791
+	// Lock: 10000 time cost(nano): 6115087
 
 }
 
