dc2e7c2e3cd82e8825cd20076ce288896b648529
李谊恒 <liyihenggnehiyil@126.com>
2017-05-18 10:22:35 +0800

    Optimize: Return statements added

--- golang/app.go
+++ golang/app.go
@@ -13,15 +13,10 @@ func main() {
 	flag.Parse()
 	if *showVersion {
 		fmt.Println(version)
+		return
 	}
 	if *showHelp {
 		flag.Usage()
-	}
-	aa := make([]int, 0,20)
-	bb := make([]int, 0,2)
-	aa = append(aa, 1)
-	aa = append(aa, 2, 3, 4)
-	if len(aa) < 20 {
-		fmt.Println(append(bb,aa[:20]...))
+		return
 	}
 }
