56542e4e9792975c4c588024552f0de50aa91f9f
李谊恒 <liyihenggnehiyil@126.com>
2017-06-05 16:28:05 +0800

    Feature add: Int64Set

--- golang/mylib/int_set.go
+++ golang/mylib/int_set.go
@@ -51,3 +51,55 @@ func (s *IntSet) Clone() *IntSet {
 	}
 	return &IntSet{data: m}
 }
+
+type Int64Set struct {
+	data map[int64]struct{}
+}
+
+func (s *Int64Set) Init() {
+	s.data = make(map[int64]struct{})
+}
+func (s *Int64Set) Size() int {
+	return len(s.data)
+}
+func (s *Int64Set) Add(n int64) bool {
+	if _, ok := s.data[n]; ok {
+		return false
+	}
+	s.data[n] = struct{}{}
+	return true
+}
+
+func (s *Int64Set) Remove(n int64) bool {
+	if _, ok := s.data[n]; ok {
+		delete(s.data, n)
+		return true
+	}
+	return false
+}
+func (s *Int64Set) Has(n int64) bool {
+	_, ok := s.data[n]
+	return ok
+}
+func (s *Int64Set) IsEmpty() bool {
+	return len(s.data) == 0
+}
+func (s *Int64Set) GetMap() map[int64]struct{} {
+	return s.data
+}
+
+func (s *Int64Set) ToSlice() []int64 {
+	ints := make([]int64, 0)
+	for i := range s.data {
+		ints = append(ints, i)
+	}
+	return ints
+}
+
+func (s *Int64Set) Clone() *Int64Set {
+	m := make(map[int64]struct{})
+	for i := range s.data {
+		m[i] = struct{}{}
+	}
+	return &Int64Set{data: m}
+}
